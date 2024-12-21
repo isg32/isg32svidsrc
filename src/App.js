e906a495411381078f38a8894cc7844a5e9d70a2
isg32 <sapangajjar101105@gmail.com>
2024-12-21 23:38:03 +0530

    isg32svidsrc: UI update (somewhat), added a button for latest TV-shows
    Signed-off-by: isg32 <sapangajjar101105@gmail.com>

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ const src= "https://vidsrc.io/embed/"
 const App = () => {
 
   const [shows,setShows] = useState([])
+  const [select,setSelect] = useState("")
   
   const getShows = async() => {
     try {
 
-      await fetch("https://proxy.cors.sh/https://api.themoviedb.org/3/discover/movie?api_key=1abb3e68d878be1155d781ce812f80a8",{
+      await fetch(`https://proxy.cors.sh/https://api.themoviedb.org/3/discover/${select ? select: "movie"}?api_key=1abb3e68d878be1155d781ce812f80a8`,{
         headers: {
           'x-cors-api-key': 'temp_6024f5608c16e2b812cc9451542813cb'
           }
@@ -27,19 +28,21 @@ const App = () => {
 
   useEffect(()=>{
     getShows()
-  },[])
+  },[select])
 
   return (
 <div className="App">
   <h1>isg32's vidsrc</h1>
   <i><h4>*it is recommended to use an adblocker*</h4></i>
   <hr/>
+  <button className='selbutton' onClick={() => setSelect("movie")}>Movies</button>
+  <button className='selbutton' onClick={() => setSelect("tv")}>TV-Shows</button>
   <div className="movies-container">
     {shows.map((data) => (
       <div className='moviecards' key={data.id}>
         <img className='posters' src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`} alt={data?.title} />
         <div className='card-headline'>
-        <p className='titles'>Title: <span>{data?.title}</span></p>
+        <p className='titles'>Title: <span>{data?.original_name}{data?.original_title}</span></p>
         <a href={src + data.id}>
           <div className='playbutton'>
             <p>Play</p>
