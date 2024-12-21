ee94ba991bc5dde15d53451622ac1efefc233142
isg32 <sapangajjar101105@gmail.com>
2024-12-21 22:49:11 +0530

    isg32svidsrc: Initial Movie latest recommendation and playback structure
    Signed-off-by: isg32 <sapangajjar101105@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const apiKey = "1abb3e68d878be1155d781ce812f80a8"
+const baseUrl = "https://api.themoviedb.org/3"
+const src= "https://vidsrc.io/embed/"
+
+const App = () => {
+
+  const [shows,setShows] = useState([])
+  
+  const getShows = async() => {
+    try {
+
+      await fetch("https://proxy.cors.sh/https://api.themoviedb.org/3/discover/movie?api_key=1abb3e68d878be1155d781ce812f80a8",{
+        headers: {
+          'x-cors-api-key': 'temp_6024f5608c16e2b812cc9451542813cb'
+          }
+      })
+      .then(res => res.json())
+      .then(json => setShows(json.results))
+      
+    } catch (er) {
+      console.error(er)
+    }
+  }
+
+  useEffect(()=>{
+    getShows()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+<div className="App">
+  <h1>isg32's vidsrc</h1>
+  <i><h4>*it is recommended to use an adblocker*</h4></i>
+  <hr/>
+  <div className="movies-container">
+    {shows.map((data) => (
+      <div className='moviecards' key={data.id}>
+        <img className='posters' src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`} alt={data?.title} />
+        <div className='card-headline'>
+        <p className='titles'>Title: <span>{data?.title}</span></p>
+        <a href={src + data.id}>
+          <div className='playbutton'>
+            <p>Play</p>
+          </div>
         </a>
-      </header>
-    </div>
+        </div>
+        <p className='titles'>Overview: <span>{data?.overview}</span></p>
+      </div>
+    ))}
+  </div>
+</div>
+
   );
 }
 
